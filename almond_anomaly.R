bdcadffa2a4414359cb6d042febf956864230a09
davesegan <davesegan@gmail.com>
2021-04-08 14:44:18 -0700

    Changed from mean(precipitation) to sum(precipitation) in all relevant lines of code
    Edited some comments to accurately represent variables

--- almond_anomaly.R
+++ almond_anomaly.R
@@ -27,8 +27,8 @@ climate_df <- read.delim(here('clim.txt'), sep = " ")
 #' @param var2_mon month for precipitation Default is 1
 #' @param coef_t1 coefficient for min temperatue Default is -0.015
 #' @param coef_t2 coefficient for squared min temperatue Default is -0.0046
-#' @param coef_p1 coefficient for min temperatue Default is -0.07
-#' @param coef_p2 coefficient for squared min temperatue Default is 0.0043
+#' @param coef_p1 coefficient for precipitation Default is -0.07
+#' @param coef_p2 coefficient for squared precipitation Default is 0.0043
 #' @param intercept Intercept of the relationship Default 0.28
 #' @references
 #' Lobell (2006).
@@ -47,29 +47,29 @@ almond_anomaly = function(climate_df,
 filt_clim_df <- climate_df %>%
    group_by(year, month) %>%
    summarize(mean_tmin = mean(tmin_c),
-             mean_p = mean(precip)) %>% 
+             sum_p = sum(precip)) %>% 
    filter(month %in% c(var1_mon,var2_mon))
 
 # first and last year
 firstyear=min(filt_clim_df$year) # starting + number of years -1
 lastyear=max(filt_clim_df$year) # starting + number of years -1
 
-# We filter the dataframe for monthly average minimum temperature
+# We filter the dataframe for the average minimum temperatures for February of each year
 clim_var1_df <- filt_clim_df %>% 
    filter(month == var1_mon) %>% 
    select(mean_tmin)
 
-# We filter the dataframe for monthly average precipitation
+# We filter the dataframe for total precipitation for January of each year
 clim_var2_df <- filt_clim_df %>% 
    filter(month == var2_mon) %>% 
-   select(mean_p)
+   select(sum_p)
 
 # We process the information and save it into a dataframe
 yield_anom <- data.frame(year= seq(firstyear, lastyear, by = 1), 
                          yield=coef_t1*clim_var1_df$mean_tmin
                             + coef_t2*(clim_var1_df$mean_tmin^2) 
-                            + coef_p1*clim_var2_df$mean_p 
-                            + coef_p2*(clim_var2_df$mean_p^2) 
+                            + coef_p1*clim_var2_df$sum_p 
+                            + coef_p2*(clim_var2_df$sum_p^2) 
                             + intercept)
 
 return(yield_anom)
